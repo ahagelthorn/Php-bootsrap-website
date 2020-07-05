65c641834d2049edb2589a1ad1f0815ddda1486f
Andrew Hagelthorn <ajhagelthorn@gmail.com>
2020-07-05 18:12:48 -0400

    Add files via upload

---
+++ projects.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark justify-content-between">
+  <!-- Brand -->
+  <a class="navbar-brand" href="index.php">
+  <i class="fas fa-home" style="color:#DDDDDD;"></i>
+  </a>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <!-- Dropdown -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="" id="navbardrop" data-toggle="dropdown">
+       <i class="fas fa-bars" style="color:#DDDDDD;"></i>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right">
+        <a class="dropdown-item" href="projects.php">Personal Projects</a>
+        <a class="dropdown-item" href="socialmedia.php">Social Media</a>
+        <a class="dropdown-item" href="codedemo.php">Code Demo</a>
+      </div>
+    </li>
+  </ul>
+</nav>
+<br>
+</body>
+</html>
+<?php
+echo "";
+?>
